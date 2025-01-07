261ea770cf7dcc843d84f30aaa7d4a2cc9888317
sandizsz <sandissirmais8@gmail.com>
2025-01-07 18:41:58 +0200

    Error fix v4

--- app/components/CasinoComponent.tsx
+++ app/components/CasinoComponent.tsx
@@ -1,6 +1,5 @@
 "use client"
 
-import React, { useState, useEffect } from 'react'
 import Image from "next/image";
 import { TypedObject } from '@portabletext/types'
 import { PortableText } from '@portabletext/react';
